b80cdd32571cc5934124ffbe570a76e6c8ff5d47
Naman <namankansal91@gmail.com>
2024-07-07 23:58:28 +0530

    added render to allowed hosts

--- analytics_backend/settings.py
+++ analytics_backend/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-62rfa_m$x=0p_+qgfk-z)!sm$tta&4(nvb2jwlr4t!unf7!$b0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://analytics-backend-odh4.onrender.com'
+]
 
 
 # Application definition
